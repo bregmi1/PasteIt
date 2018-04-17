56a823c05809f4e8b8f2a1291cdc878400b98705
Bijay Regmi <regmibijay2@gmail.com>
2018-04-17 02:18:52 -0500

    Adds a migration script to add the dummy values to the users and pastes tables

---
+++ src/main/resources/db/migration/V0002__InsertDummyDataIntoUsersAndPastesTable.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, password) VALUES
+('Bijay', 'bregmi1@uno.edu', 'password123A'),
+('John Doe', 'john@doe.com', 'dummyPassword');
+
+INSERT INTO pastes (body, user_id) VALUES
+('This is the first paste of Bijay Regmi', 1),
+('This is the second paste of Bijay Regmi', 1),
+('This is the first paste of John Doe', 2);
