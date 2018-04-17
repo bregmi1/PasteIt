40c5d4c4632d41b6a1082dd2f313c5cfccb839b9
Bijay Regmi <regmibijay2@gmail.com>
2018-04-17 02:18:16 -0500

    Creates a migration to create users and pastes table

---
+++ src/main/resources/db/migration/V0001__CreatePastesAndUsersTable.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+  user_id BIGSERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  created_on TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_on TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+ALTER TABLE users ADD CONSTRAINT chk_updated_on_valid CHECK (created_on <= updated_on);
+
+CREATE TABLE pastes (
+  paste_id BIGSERIAL PRIMARY KEY,
+  body TEXT NOT NULL,
+  user_id BIGINT REFERENCES users (user_id),
+  expires_on TIMESTAMP WITH TIME ZONE,
+  created_on TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_on TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+ALTER TABLE pastes ADD CONSTRAINT chk_updated_on_valid CHECK (created_on <= updated_on);
+ALTER TABLE pastes ADD CONSTRAINT chk_expires_on_valid CHECK (expires_on >= created_on);
