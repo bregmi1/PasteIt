0d408aa8d3c8c7a43215167a0ae70f7f216e47a0
Bijay Regmi <regmibijay2@gmail.com>
2018-04-18 00:48:11 -0500

    Creates a resource accessor for the model Paste

---
+++ src/main/java/com/regmi/bijay/pasteit/accessors/IPasteAccessor.java
@@ -0,0 +1,17 @@
+package com.regmi.bijay.pasteit.accessors;
+
+import com.regmi.bijay.pasteit.domains.Paste;
+import com.regmi.bijay.pasteit.domains.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface IPasteAccessor extends JpaRepository<Paste, Long> {
+
+    List<Paste> findAllByUser(User user);
+
+    List<Paste> findAllByExpiresOnAfterAndExpiresOnBefore(LocalDateTime startDate, LocalDateTime endDate);
+
+    List<Paste> findAllByExpiresOnBefore(LocalDateTime endDate);
+}
