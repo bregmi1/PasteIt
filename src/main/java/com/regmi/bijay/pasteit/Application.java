bd4293a55e7e36a42b749a7e768ecd1f898caf30
Bijay Regmi <regmibijay2@gmail.com>
2018-04-18 05:54:45 -0500

    Tests the accessors by using CommandLineRunner

--- src/main/java/com/regmi/bijay/pasteit/Application.java
+++ src/main/java/com/regmi/bijay/pasteit/Application.java
@@ -12,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
+
 
 @SpringBootApplication
 public class Application {
@@ -31,15 +33,40 @@ public class Application {
                 user.setName("Test User ");
                 user.setEmail("test@example.com");
                 user.setPassword("password123A");
+
+                LocalDateTime now = LocalDateTime.now();
+                user.setCreatedOn(now);
+                user.setUpdateOn(now);
                 LOGGER.info("Created new user: {}", user);
 
                 Paste paste = new Paste();
                 paste.setBody("This is the body of the paste");
                 paste.setUser(user);
+                paste.setCreatedOn(now);
+                paste.setUpdatedOn(now);
+                paste.setExpiresOn(now.plusDays(1));
                 LOGGER.info("Created new Paste: {}", paste);
 
                 LOGGER.info("All pastes: " + pasteAccessor.findAll());
                 LOGGER.info("All users: " + userAccessor.findAll());
+
+                userAccessor.save(user);
+                LOGGER.info("All users after adding test user: " + userAccessor.findAll());
+
+                pasteAccessor.save(paste);
+                LOGGER.info("All pastes after adding the test paste: " + pasteAccessor.findAll());
+
+                LOGGER.info("All the pastes by test user: " + pasteAccessor.findAllByUser(user));
+
+                LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+                LocalDateTime tomorrow = LocalDateTime.now().plusDays(1);
+
+                LOGGER.info("All the pastes that expires between yesterday and tomorrow: " + pasteAccessor.findAllByExpiresOnAfterAndExpiresOnBefore(yesterday, tomorrow));
+                LOGGER.info("All the pastes that expires before tomorrow: " + pasteAccessor.findAllByExpiresOnBefore(tomorrow));
+
+                LOGGER.info("The user with email test@example.com: " + userAccessor.findByEmail("test@example.com"));
+                LOGGER.info("All the users created between yesterday and tomorrow: " + userAccessor.findAllByCreatedOnAfterAndCreatedOnBefore(yesterday, tomorrow));
+
             }
         };
     }
--- src/main/resources/db/migration/V0002__InsertDummyDataIntoUsersAndPastesTable.sql
+++ src/main/resources/db/migration/V0002__InsertDummyDataIntoUsersAndPastesTable.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (name, email, password) VALUES
-('Bijay', 'bregmi1@uno.edu', 'password123A'),
+('Harry', 'harry@example.com', 'password123A'),
 ('John Doe', 'john@doe.com', 'dummyPassword');
 
 INSERT INTO pastes (body, user_id) VALUES
-('This is the first paste of Bijay Regmi', 1),
-('This is the second paste of Bijay Regmi', 1),
+('This is the first paste of Harry', 1),
+('This is the second paste of Harry', 1),
 ('This is the first paste of John Doe', 2);
