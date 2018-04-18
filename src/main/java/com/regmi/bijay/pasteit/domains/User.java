ec88c3635c90af74fadb6fa8969ae02cf2290aaa
Bijay Regmi <regmibijay2@gmail.com>
2018-04-17 22:49:50 -0500

    Provides the POJO implementation of the User model

--- src/main/java/com/regmi/bijay/pasteit/domains/User.java
+++ src/main/java/com/regmi/bijay/pasteit/domains/User.java
@@ -1,4 +1,124 @@
 package com.regmi.bijay.pasteit.domains;
 
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+
+    @Column(name = "updated_on")
+    private LocalDateTime updateOn;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<Paste> pastes;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public LocalDateTime getUpdateOn() {
+        return updateOn;
+    }
+
+    public void setUpdateOn(LocalDateTime updateOn) {
+        this.updateOn = updateOn;
+    }
+
+    public Set<Paste> getPastes() {
+        return pastes;
+    }
+
+    public void setPastes(Set<Paste> pastes) {
+        this.pastes = pastes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(createdOn, user.createdOn) &&
+                Objects.equals(updateOn, user.updateOn) &&
+                Objects.equals(pastes, user.pastes);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(userId, name, email, password, createdOn, updateOn, pastes);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", createdOn=" + createdOn +
+                ", updateOn=" + updateOn +
+                ", pastes=" + pastes +
+                '}';
+    }
 }
