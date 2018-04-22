1b575649b1fad537153fa0e41bb5d38852dd4da5
Bijay Regmi <regmibijay2@gmail.com>
2018-04-22 02:14:11 -0500

    Provides the implementation of IUserConverter.java

---
+++ src/main/java/com/regmi/bijay/pasteit/converters/impl/UserConverter.java
@@ -0,0 +1,44 @@
+package com.regmi.bijay.pasteit.converters.impl;
+
+import com.regmi.bijay.pasteit.converters.ILocalDateTimeConverter;
+import com.regmi.bijay.pasteit.converters.IUserConverter;
+import com.regmi.bijay.pasteit.domains.DomainUser;
+import com.regmi.bijay.pasteit.views.ViewUser;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class UserConverter implements IUserConverter {
+
+    @Autowired
+    private ILocalDateTimeConverter localDateTimeConverter;
+
+    @Override
+    public DomainUser viewToDomain(ViewUser viewUser) {
+        DomainUser domainUser = new DomainUser();
+        domainUser.setUserId(viewUser.getUserId());
+        domainUser.setEmail(viewUser.getEmail());
+        domainUser.setName(viewUser.getName());
+        domainUser.setPassword(viewUser.getPassword());
+
+        LocalDateTime now = LocalDateTime.now();
+        domainUser.setUpdateOn(now);
+
+        if(viewUser.getUserId() == null){
+            domainUser.setCreatedOn(now);
+        } else{
+            domainUser.setCreatedOn(localDateTimeConverter.convertLongToLocalDateTime(viewUser.getCreatedOn()));
+        }
+        return domainUser;
+    }
+
+    @Override
+    public ViewUser domainToView(DomainUser domainUser) {
+        ViewUser viewUser = new ViewUser();
+        viewUser.setUserId(viewUser.getUserId());
+        viewUser.setName(domainUser.getName());
+        viewUser.setEmail(domainUser.getEmail());
+        viewUser.setCreatedOn(localDateTimeConverter.convertLocalDateTimeToLong(domainUser.getCreatedOn()));
+        return viewUser;
+    }
+}
