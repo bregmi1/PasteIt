ce5a71a4f984e83266e586786d0e325a940c2d94
Bijay Regmi <regmibijay2@gmail.com>
2018-04-22 02:14:00 -0500

    Provides the implementation of IPasteConverter.java

---
+++ src/main/java/com/regmi/bijay/pasteit/converters/impl/PasteConverter.java
@@ -0,0 +1,45 @@
+package com.regmi.bijay.pasteit.converters.impl;
+
+import com.regmi.bijay.pasteit.converters.ILocalDateTimeConverter;
+import com.regmi.bijay.pasteit.converters.IPasteConverter;
+import com.regmi.bijay.pasteit.domains.DomainPaste;
+import com.regmi.bijay.pasteit.views.ViewPaste;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class PasteConverter implements IPasteConverter {
+
+    @Autowired
+    private ILocalDateTimeConverter localDateTimeConverter;
+
+
+    @Override
+    public DomainPaste viewToDomain(ViewPaste viewPaste) {
+        DomainPaste domainPaste = new DomainPaste();
+        domainPaste.setPasteId(viewPaste.getPasteId());
+        domainPaste.setBody(viewPaste.getBody());
+        domainPaste.setUserId(viewPaste.getUserId());
+        domainPaste.setExpiresOn(localDateTimeConverter.convertLongToLocalDateTime(viewPaste.getExpiresOn()));
+        LocalDateTime now = LocalDateTime.now();
+        domainPaste.setUpdatedOn(now);
+
+        if(viewPaste.getPasteId() == null){
+            domainPaste.setCreatedOn(now);
+        } else{
+            domainPaste.setCreatedOn(localDateTimeConverter.convertLongToLocalDateTime(viewPaste.getCreatedOn()));
+        }
+        return domainPaste;
+    }
+
+    @Override
+    public ViewPaste domainToView(DomainPaste domainPaste) {
+        ViewPaste viewPaste = new ViewPaste();
+        viewPaste.setPasteId(domainPaste.getPasteId());
+        viewPaste.setBody(domainPaste.getBody());
+        viewPaste.setExpiresOn(localDateTimeConverter.convertLocalDateTimeToLong(domainPaste.getExpiresOn()));
+        viewPaste.setUpdatedOn(localDateTimeConverter.convertLocalDateTimeToLong(domainPaste.getUpdatedOn()));
+        viewPaste.setUserId(domainPaste.getUserId());
+        return viewPaste;
+    }
+}
